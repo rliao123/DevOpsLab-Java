1cee50bb54d60781bbd1ced7d63f9eb8600b4f90
rliao123 <92598518+rliao123@users.noreply.github.com>
2024-04-24 18:35:00 -0700

    update files

--- src/main/java/edu/sjsu/NameChecker.java
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -1,10 +1,13 @@
 package edu.sjsu;
 
+import java.util.regex.*;
+
 public class NameChecker {
     /**
      * Checks if a given string is a valid name following these rules:
      * - Between 2 to 40 characters
-     * - Contains only alphabetic characters, non-consecutive hyphens, or a single quote
+     * - Contains only alphabetic characters, non-consecutive hyphens, or a single
+     * quote
      * - Cannot start with a hyphen or single quote
      * 
      * @param input Name to check
@@ -13,6 +16,13 @@ public class NameChecker {
     public static boolean check(String input) {
         // TODO: implement
 
-        return false;
+        String regex = "^(?!['-])(?!.*['-]{2})[a-zA-Z'-]{2,40}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
